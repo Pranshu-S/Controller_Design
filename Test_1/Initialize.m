64be14d526d8b14560d9b670e382883d786ed569
Pranshu Shukla <55992439+Pranshu-S@users.noreply.github.com>
2021-05-12 17:54:16 +0530

    SetupVals

--- Test_1/Initialize.m
+++ Test_1/Initialize.m
@@ -16,10 +16,19 @@ B = [0;1];
 C = [1,0];
 D = 0;
 
-%% LQR
 
-Q = eye(2);
-R = 1;
-K = lqr(A,B,Q,R);
-G = -inv(C*inv(A-B*K)*B);
+%% Check for Stability 
+Eigenvalues_of_A = eig(A)
+Controllability_matrix = ctrb(A,B)
+Controllability_rank = rank(Controllability_matrix)
+Observability_matrix = obsv(A,C)
+Observability_rank = rank(Observability_matrix)
+
+%% LQR
+% 
+% Q = [0.5, 0; 0, 0.2];
+% R = 0.5;
+% K = lqr(A,B,Q,R);
+% G = C*inv(A-B*K)*B
+% % G = -inv(C*inv(A-B*K)*B);
 
