beb869f7001e797526e0de615eb3a9404bf830b0
Pranshu Shukla <55992439+Pranshu-S@users.noreply.github.com>
2021-06-08 07:51:38 +0530

    Made changes

--- LQR + Observer/kalman_observer_params.m
+++ LQR + Observer/kalman_observer_params.m
@@ -10,44 +10,25 @@ D = [0;0];
 
 
 % Controller design
-Q = eye(2)*3;
-R = 3;
+Q = eye(2)*100;
+R = 0.00001;
 M_a = rank(ctrb(A,B));
 P = care(A,B,Q,R);
 K_u  = inv(R)*B'*P;
 
-t = 0:0.001:5; 
-dt = t(2) - t(1);
-X(:,1) = [1;0];
-y(:,1) = C*X;
+% Sampling Time
+dt = 1/1000;
 
 % Observer design
-Q_k = eye(2)*5;
-R_k = eye(2)*0.00846698*100;
+Q_k = eye(2);
+R_k = diag([0.00846698/4000,0.00846698/4000]);
 P_k = care(A',C',Q_k,R_k);
 K_k = P_k*C'*inv(R_k);
 L = K_k;
 
-% X_hat(:,1) = [0;0];
-% y_hat(:,1) = C*X_hat;
-% for i = 2:length(t)
-%     u = -K_u*X_hat(:,i-1);
-%     
-%     X(:,i) = X(:,i-1)  +dt * (A*X(:,i-1) + B*u);
-%     y(:,i) = C*X(:,i) + sqrt(R_k)*randn(size(C,1),1);
-% 
-%     X_hat(:,i) = X_hat(:,i-1)  +dt * (A*X_hat(:,i-1) + B*u +L *(y(:,i-1)-y_hat(:,i-1)));
-%     y_hat(:,i) = C*X_hat(:,i) ;
-% end
-% 
-% 
-% figure;
-% subplot(2,1,1)
-% plot(t,X(1,:),t,X_hat(1,:))
-% legend('Actual','Estimate')
-% xlabel('time')
-% ylabel('Position')
-% subplot(2,1,2)
-% plot(t,X(2,:),t,X_hat(2,:))
-% xlabel('time')
-% ylabel('Velocity')
+% White noise characteristics
+R_w = 0.00846698/4000*dt;
+
+% Desired State
+des_x = [0.0005; 0];
+des_u = K_u*des_x;
